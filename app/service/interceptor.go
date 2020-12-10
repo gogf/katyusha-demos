b92a7965e447bf76d9d1b782a149bf563d69b30b
John Guo <john@johng.cn>
2020-12-11 01:19:00 +0800

    up

--- app/service/interceptor.go
+++ app/service/interceptor.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gogf/gf/util/gvalid"
 	"github.com/gogf/katyusha-demos/app/model"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
 // 拦截器，类似中间件作用。
@@ -46,7 +47,10 @@ func (s *serviceInterceptor) UnaryCtx(ctx context.Context, req interface{}, info
 func (s *serviceInterceptor) UnaryValidate(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	// 如果没有StructTag，那么校验器什么都不会做
 	if err := gvalid.CheckStruct(req, nil); err != nil {
-		return nil, gerror.Current(err)
+		return nil, gerror.NewCode(
+			int(codes.InvalidArgument),
+			gerror.Current(err).Error(),
+		)
 	}
 	return handler(ctx, req)
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/gogf/katyusha-demos
 go 1.11
 
 require (
-	github.com/gogf/gf v1.14.6-0.20201209080429-7316e6648fe3
-	github.com/gogf/katyusha v0.0.0-20201209083811-06412389062a
+	github.com/gogf/gf v1.14.6-0.20201210171253-363f6eba4454
+	github.com/gogf/katyusha v0.0.0-20201210171714-50a4d6c5c88f
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/protobuf v1.4.3
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2 // indirect
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
 	google.golang.org/grpc v1.29.1
 )
